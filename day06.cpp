1a414c1ea9093bb413fd9dfd467188e6bb7d50b6
Nejc Zajc <nz44156@student.uni-lj.si>
2023-12-06 06:46:50 +0100

    day 06

---
+++ day06.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+
+int main() {
+    
+    string line;
+    vector< long long int > times = {50, 74, 86, 85, 50748685};
+    vector< long long int > records = {242, 1017, 1691, 1252, 242101716911252};
+    int solA = 1;
+
+    for (int i = 0; i < 5; i++) {
+        long long int T = times[i];
+        long long int wins = 0;
+
+        // --------------------------------------------
+        // // this doesn't work for b) part...
+        // float t1 = (T + sqrt(T*T - 4*records[i]))/2;
+        // float t2 = (T - sqrt(T*T - 4*records[i]))/2;
+        // int t1Int = floor(t1);
+        // int t2Int = ceil(t2);
+        // int wins = t1Int - t2Int + 1;
+        // --------------------------------------------
+        for (long long int charging = 0; charging < T; charging++) {
+            long long int result = (T-charging)*charging;
+            if (result > records[i]) {
+                wins += 1;
+            }
+        }
+        if (i < 4) {
+            solA *= wins;
+        }
+        else {
+            cout << "b: " << wins << endl;
+        }
+    }
+    cout << "a: " << solA << endl;
+
+    return 0;
+}
